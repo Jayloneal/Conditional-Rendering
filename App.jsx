9fcd2712665999523766b118a5cdd2cee669df82
Jayloneal <121124684+Jayloneal@users.noreply.github.com>
2023-04-09 08:32:30 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
+
+const currentTime = new Date(4, 1, 2023, 17).getHours()
+
+console.log(currentTime);
+
+function App() {
+  return <div className="container">{
+    // isLoggedIn ?
+    // <h1>Hello</h1> :
+    //    <Login />
+
+    currentTime > 12 ?
+      <h1>Why are you still working?</h1> : null
+  
+  }</div>;
+}
+
+export default App;
